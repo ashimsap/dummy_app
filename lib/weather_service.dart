106cfdeb21fc6a1275160bf6972fd4be7bb5fa72
Ashim <ashimsap@gmail.com>
2025-12-21 09:51:14 +0545

    rebuild app for practice.

--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 import 'weather_model.dart';
 
 class WeatherService {
-  final String apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
-  final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  final String apiKey = '29c443132feb5ac3f53bb9b1fb9d0407'; // Replace with your OpenWeatherMap API key
+  final String baseUrl = 'https://api.openweathermap.org/data/2.0/weather';
 
   Future<Weather> getWeather(String cityName) async {
     final response = await http.get(Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric'));
